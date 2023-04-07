a04a7949dbe9029b825bd534f47c38fd51281c0d
thistechguy <86990020+thistechguy@users.noreply.github.com>
2023-04-07 14:58:48 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,34 @@
+#!/bin/bash
+
+# Update the system
+
+zypper ref
+zypper update
+
+# Install Packman repos
+zypper ar -cfp 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
+zypper dup --from packman --allow-vendor-change
+
+# Instals OPI
+zypper install opi
+
+# Install codecs
+opi codecs
+
+# Install Microsoft Edge
+flatpak install flathub com.microsoft.Edge
+
+# Install Discord
+flatpak install flathub com.discordapp.Discord
+
+# Install Microsoft Fonts and Build Essentials
+zypper install fetchmsttfonts patterns-devel-base-devel_basis
+
+# Install VSCode
+zypper ar https://packages.microsoft.com/yumrepos/vscode vscode
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+zypper refresh
+zypper install code
+
+read -p "All done! Press enter to continue"
 
